fa7ac1a0f9daf91ae3f5672fda7bab15ac7fbc0f
Guled12345 <g.warsameh@alustudent.com>
2024-12-04 09:07:40 +0200

    summative

--- education_access_app/app.py
+++ education_access_app/app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+from model_utils import load_model
 from home import home_page
 from prediction import prediction_page
 from retrain_model import retrain_page
+import sys
+print(sys.path)
+
 
 # Sidebar navigation
 st.sidebar.title("Navigation")
